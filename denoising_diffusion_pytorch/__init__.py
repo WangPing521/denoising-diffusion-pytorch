b91c6a75c33a4e8a3038d0c02bd5a0b9e70ab0bc
jizong <jizong.peng.1@etsmtl.net>
2022-06-05 18:04:47 -0400

    extracted unet as seperate file

--- denoising_diffusion_pytorch/__init__.py
+++ denoising_diffusion_pytorch/__init__.py
@@ -1,4 +1,12 @@
-from denoising_diffusion_pytorch.denoising_diffusion_pytorch import GaussianDiffusion, Unet, Trainer
+from denoising_diffusion_pytorch.denoising_diffusion_pytorch import (
+    GaussianDiffusion,
+    Trainer,
+)
+from denoising_diffusion_pytorch.unet import Unet
 
-from denoising_diffusion_pytorch.learned_gaussian_diffusion import LearnedGaussianDiffusion
-from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import WeightedObjectiveGaussianDiffusion
+from denoising_diffusion_pytorch.learned_gaussian_diffusion import (
+    LearnedGaussianDiffusion,
+)
+from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import (
+    WeightedObjectiveGaussianDiffusion,
+)
